862b8bbd0685968857e52f169a26ff6f6d9b0130
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-08-22 16:04:21 -0300

    Agregar efecto blur al navbar

--- frontend-nextjs14/src/app/panel/layout.tsx
+++ frontend-nextjs14/src/app/panel/layout.tsx
@@ -1,12 +1,20 @@
+"use client"
 import { Navbar } from "@/components/Navbar";
 import styles from "@/styleModules/generalModules.module.css";
+import { PropertiesHome } from "@/types/TypesHome";
+import { useSelector } from "react-redux";
 
 export default function LayoutAdmin({ children }:{children:React.ReactNode}) {
+
+  const { blur } = useSelector((state:PropertiesHome) => state.properties );
+  
     return(
       <>
         <div className={styles.backgroundPanel}>
           <Navbar />
-          { children }
+          <div className={(blur) ? (styles.activeBlur) : ("")}>
+            { children }
+          </div>
         </div>
       </>
     )
