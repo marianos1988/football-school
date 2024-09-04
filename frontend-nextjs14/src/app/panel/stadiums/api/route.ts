c34be56a468366be7621a66cc29b945b0d4c3665
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-09-04 17:28:56 -0300

    Corregir validacion de parametros Stadiums

--- frontend-nextjs14/src/app/panel/stadiums/api/route.ts
+++ frontend-nextjs14/src/app/panel/stadiums/api/route.ts
@@ -3,18 +3,23 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   
-  const parameters = parametersStadiums;
-  
-  // if(!(parametersStadiums.listStadiums[0].id === 0) || !(parametersStadiums.listStadiums[0].idUser === 0) || !(parametersStadiums.listStadiums[0].name === "") || !(parametersStadiums.listStadiums[0].description === "") ) {
 
-  //   return NextResponse.json(parameters);
-
-  // } else {
+  if((parametersStadiums.listStadiums[0].id === 0) && (parametersStadiums.listStadiums[0].idUser === 0) && (parametersStadiums.listStadiums[0].name === "") && (parametersStadiums.listStadiums[0].description === "") ) {
+   
     const response = await fetch("http://localhost:3000/Stadiums/AllStadiums/");
     const listStadiums = await response.json();
-    console.log(listStadiums);
 
-    return NextResponse.json(listStadiums)
-  // }
+
+  
+    parametersStadiums.listStadiums.shift();
+    parametersStadiums.listStadiums.push(listStadiums);
+
+    return NextResponse.json(parametersStadiums.listStadiums[0])
+
+  } else {
+
+
+    return NextResponse.json(parametersStadiums.listStadiums[0]);
+  }
 
 }
