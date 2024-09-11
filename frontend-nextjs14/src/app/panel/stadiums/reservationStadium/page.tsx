1c9397afd2b53c278b64587ac1adabc60e5d4da3
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-09-11 10:51:08 -0300

    Agregar validarLogin modulo reservar cancha

--- frontend-nextjs14/src/app/panel/stadiums/reservationStadium/page.tsx
+++ frontend-nextjs14/src/app/panel/stadiums/reservationStadium/page.tsx
@@ -1,53 +1,54 @@
 "use client";
-import { useSelector } from "react-redux";
 import "@/styles/ReservationStadium.css";
 import { useUtils } from "@/hooks/useUtils";
 import { useEffect } from "react";
 import { PropertiesHome } from "@/types/TypesHome";
 import { FormReservation } from "@/components/FormReservation";
 import { CardStadium } from "@/components/CardStadium";
+import { useRouter } from "next/navigation";
 
 
 export default function ReservationStadium() {
+  const { checkLogin } = useUtils();
+  const route = useRouter();
+
+  const checkLoginPage = async () =>{
+
+    const validation = await checkLogin();
+
+    if(!validation) {
+      route.push("/auth/login");
+    } else {
+
+      try {
+        //Traer id de la cancha
+        const response = await fetch("http://localhost:3001/panel/stadiums/api/");
+        const newListStadiums = await response.json();
+
+      } catch (error) {
+
+        console.log(error)
+      }
+    }
+  }
+
+  useEffect(()=>{
+    checkLoginPage();
+  },[]);
+
   return (
     <>
-
-    <div className="container-reservation-stadium">
-      <h1>Iniciar Reserva</h1>
-      <div className="box-form">
-        <CardStadium
-          id={1}
-          reservation={true} id_user={0} name={""} description={""} numberStadium={0} typeStadium={0}
-        />
-        <FormReservation /> 
-      </div> 
-    </div>
-
-  </>
+      <div className="container-reservation-stadium">
+        <h1>Iniciar Reserva</h1>
+        <div className="box-form">
+          <CardStadium
+            id={1}
+            reservation={true} id_user={0} name={""} description={""} numberStadium={0} typeStadium={0}
+          />
+          <FormReservation /> 
+        </div> 
+      </div>
+    </>
   )
 }
 
-// export const ReservationStadium = () => {
-// const { checkLogin } = useUtils();
-// // useEffect(()=>{checkLogin()});
-
-// const { idStadium } = useSelector((state:{reservationStadium: {idStadium: number}}) => state.reservationStadium);
-// const { blur }= useSelector((state:PropertiesHome) =>  state.properties);
-
-//   return ( 
-//     <>
-
-//       <div className="container-reservation-stadium">
-//         <h1>Iniciar Reserva</h1>
-//         <div className="box-form">
-//           <CardStadium
-//             id={1}
-//             reservation={true} id_user={0} name={""} description={""} numberStadium={0} typeStadium={0}
-//           />
-//           <FormReservation /> 
-//         </div> 
-//       </div>
-
-//     </>
-//   )
-// }
