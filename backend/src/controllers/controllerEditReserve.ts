794e08e03dcf7ba20f48896de28c9c8a055e6d10
Mariano SY <138610830+marianos1988@users.noreply.github.com>
2024-11-13 17:05:36 -0300

    Agregar async await en query en controllerEditReserve

--- backend/src/controllers/controllerEditReserve.ts
+++ backend/src/controllers/controllerEditReserve.ts
@@ -36,12 +36,15 @@ const editReserve = async (req:any, res:any) => {
     
 
             let query = `SELECT * FROM reservas WHERE id = ${parametersInitialEditReserve[0].idReserve} AND id_stadium = ${parametersInitialEditReserve[0].idStadium} AND id_user = ${parametersInitialEditReserve[0].idUser}`;
-            pool.query(query, ((err,resu) =>{
+            pool.query(query, ( async (err,resu) =>{
 
                 try {
+                    const newResu = await resu;
+                    console.log(newResu)
+
                     if(err) throw err;
 
-                    if(resu < 1) {
+                    if(newResu < 1) {
                         res.json({
                             parametersInitialEditReserve: {
                                 idReserve: parseData.idReserve,
@@ -55,20 +58,20 @@ const editReserve = async (req:any, res:any) => {
                         });
                     } else {
 
-                        const dateFinal = utils.getFullDate(resu[0].fecha_reserva);
-                        const timeFinal = utils.getFullTime(resu[0].hora_reserva);
+                        const dateFinal = utils.getFullDate(newResu[0].fecha_reserva);
+                        const timeFinal = utils.getFullTime(newResu[0].hora_reserva);
         
                         let object = {
-                            idReserve: resu[0].id,
-                            idUser: resu[0].id_user,
-                            idStadium: resu[0].id_stadium,
-                            numberStadium: resu[0].number_stadium,
-                            nameClient: resu[0].cliente,
-                            phone: resu[0].telefono,
-                            email: resu[0].email,
+                            idReserve: newResu[0].id,
+                            idUser: newResu[0].id_user,
+                            idStadium: newResu[0].id_stadium,
+                            numberStadium: newResu[0].number_stadium,
+                            nameClient: newResu[0].cliente,
+                            phone: newResu[0].telefono,
+                            email: newResu[0].email,
                             date: dateFinal,
                             time: timeFinal,
-                            cash: resu[0].senia
+                            cash: newResu[0].senia
                         }
 
                         parametersEditReserve.shift();
